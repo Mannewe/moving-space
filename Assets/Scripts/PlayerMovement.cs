5efb61bdd9522277736b84eed0b53203208b2a33
Manne Westermark <mannewestermark@gmail.com>
2016-07-28 12:44:23 +0200

    Added weapon2 method

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,45 +4,57 @@ using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour {
 
-	public bool shooter = false;
 	Animator anim;
+
 	float input;
+	float currentSpeed;
+
+	float horizontal;
+	float vertical;
+
+	bool shooting = true;
+
+	Rigidbody2D[] bullet;
+	Text livesText;
+
+	public bool shooter = false;
 	public float speed;
 	public float turnSpeed;
 	public float projectileSpeed;
 	public float boost = 10;
-	float currentSpeed;
-	float horizontal;
-	float vertical;
+	public float fireRate;
 	public float lives = 5;
+
 	public Vector3 currentPos;
-	public Rigidbody2D projectile;
-	Rigidbody2D bullet;
-	Text livesText;
+
+	public Rigidbody2D projectile1;
 
 
 
 	void Awake()
 	{
+		bullet = new Rigidbody2D[3];
 		anim = gameObject.GetComponent<Animator> ();
 		livesText = GameObject.Find ("Text").GetComponent<Text>();
 	}
 
+	void Start(){
+	}
+
 	void Update()
 	{	
+		print (shooting);
 		currentPos = transform.position;
-		print (currentPos);
 
 		if(input > 0 || input < 0){
 			anim.SetFloat("speed", input);
 		}
 			
-		if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space)) {
+		if (Input.GetMouseButton(0) || Input.GetKey(KeyCode.Space)) {
 			Shoot ();
 		}
 
 		if (Input.GetKey (KeyCode.LeftShift) && boost > 0) {
-			print (boost);
 			boost -= Time.deltaTime;
 			speed = 80;
 		}
@@ -70,18 +82,59 @@ public class PlayerMovement : MonoBehaviour {
 	}
 
 	void Shoot()
-	{
-		bullet = (Rigidbody2D) Instantiate (projectile, transform.position, transform.rotation);
-		if(input <= 0.2 && input >= - 0.2)
-		bullet.velocity = transform.up * (projectileSpeed);
-		else if(input > 0.2 && !Input.GetKey(KeyCode.LeftShift) || input < - 0.2 && !Input.GetKey(KeyCode.LeftShift))
-			bullet.velocity = transform.up * (projectileSpeed + 7);
-		else if(Input.GetKey(KeyCode.LeftShift))
-			bullet.velocity = transform.up * (projectileSpeed + 17);
+	{	
+		if (shooting == true) {
+			Weapon1 ();
+		}
 	}
 
 	public void LooseLife()
 	{
 		lives--;
+	}
+
+	void AbleToShoot()
+	{
+		shooting = true;
+	}
+
+
+	void Weapon1()
+	{
+		bullet[0] = (Rigidbody2D)Instantiate (projectile1, transform.position, transform.rotation);
+		if (input <= 0.2 && input >= -0.2)
+			bullet[0].velocity = transform.up * (projectileSpeed);
+		else if (input > 0.2 && !Input.GetKey (KeyCode.LeftShift) || input < -0.2 && !Input.GetKey (KeyCode.LeftShift))
+			bullet[0].velocity = transform.up * (projectileSpeed + 7);
+		else if (Input.GetKey (KeyCode.LeftShift))
+			bullet[0].velocity = transform.up * (projectileSpeed + 17);
+		Invoke ("AbleToShoot",fireRate);
+		shooting = false;
+	}
+
+	void Weapon2()
+	{
+
+		bullet[0] = (Rigidbody2D)Instantiate (projectile1, transform.position, transform.rotation);
+		bullet [1] = (Rigidbody2D)Instantiate (projectile1, transform.position, transform.rotation);
+		bullet[2] = (Rigidbody2D)Instantiate (projectile1, transform.position, transform.rotation);
+
+		bullet [0].transform.Rotate (0,0,10);
+		bullet [1].transform.Rotate (0, 0, -10);
+
+		if (input <= 0.2 && input >= -0.2)
+			for (int i = 0; i < bullet.Length; i++) {
+				bullet [i].velocity = bullet[i].transform.up * (projectileSpeed);
+			}
+		else if (input > 0.2 && !Input.GetKey (KeyCode.LeftShift) || input < -0.2 && !Input.GetKey (KeyCode.LeftShift))
+			for (int i = 0; i < bullet.Length; i++) {
+				bullet [i].velocity = bullet[i].transform.up * (projectileSpeed + 7);
+			}
+		else if (Input.GetKey (KeyCode.LeftShift))
+			for (int i = 0; i < bullet.Length; i++) {
+				bullet[i].velocity = bullet[i].transform.up * (projectileSpeed + 17);
+			}
+		Invoke ("AbleToShoot",fireRate);
+		shooting = false;
 	}
 }
